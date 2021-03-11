e4a1b83efca1bdc4c9691f8fbd86c2fd162de3dc
oscarporras02 <oscarrussi@outlook.com>
2021-03-11 13:01:25 -0500

    filter is working

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,41 +2,48 @@ import { nanoid } from 'nanoid';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import CategoryFilter from '../components/CategoryFilter';
 
 function BooksList(props) {
-  const { books } = props;
+  const { books, category } = props;
 
   return (
-    <table className="table table-bordered">
-      <thead className="thead-dark">
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          books.map(book => (
+    <div>
+      <CategoryFilter />
+      <table className="table table-bordered">
+        <thead className="thead-dark">
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+          books.filter(book => book.category === category || category === 'All').map(book => (
             <Book book={book} key={nanoid()} />
           ))
       }
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
 const mapStateToProps = state => ({
   books: state.book.books,
+  category: state.filter.category,
 });
 
 BooksList.propTypes = {
   books: PropTypes.shape([]),
+  category: PropTypes.string,
 };
 
 BooksList.defaultProps = {
   books: null,
+  category: 'All',
 };
 
 export default connect(mapStateToProps)(BooksList);
