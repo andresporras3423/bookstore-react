3dee6ed59ad511e51b5e5908d84dda4240b79db7
oscarporras02 <oscarrussi@outlook.com>
2021-03-12 14:06:04 -0500

    added navbar

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,16 @@ import BooksList from '../containers/BooksList';
 function App() {
   return (
     <div className="App">
-      <BooksForm />
+      <nav className="panel-bg">
+        <h1 className="Bookstore-CMS">Bookstore CMS</h1>
+        <span className="BOOKS">BOOKS</span>
+        <span className="CATEGORIES">CATEGORIES</span>
+        <span className="Oval">
+          <span className="Mask" />
+        </span>
+      </nav>
       <BooksList />
+      <BooksForm />
     </div>
   );
 }
